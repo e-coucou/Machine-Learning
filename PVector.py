f7954cb4bc9c51704bf3a2994012086701e86671
e-Coucou <eric@e-coucou.com>
2022-08-29 00:38:01 +0200

    updt

--- PVector.py
+++ PVector.py
@@ -1,35 +1,37 @@
-from msilib.schema import Class
-
+import numpy as np
+from math import *
+# from msilib.schema import Class
+# 
 
 class PVector():
     def __init__(self,x,y) -> None:
         self.vector = np.array([x,y])
 
-    def setHeading(v,a,mag=1):
+    def setHeading(self,v,a,mag=1):
         return (np.array([mag*cos(a),-mag*sin(a)]) + v).astype(int)
 
-    def heading(v):
+    def heading(self,v):
         return (np.arctan2(-v[1],v[0]))
 
-    def setMag(v,mag):
+    def setMag(self,v,mag):
         coef = np.linalg.norm(v)
         return (v/coef*mag)
 
-    def norm(v):
+    def norm(self,v):
         coef = np.linalg.norm(v)
         return (v/coef)
 
-    def dist(v1,v2):
+    def dist(self,v1,v2):
         d = np.linalg.norm(v2-v1)
         return d
 
-    def limit(v,lim):
-        return norm(v)*lim
+    def limit(self,v,lim):
+        return self.norm(v)*lim
 
-    def findProjection(pos,a ,b):
+    def findProjection(self,pos,a ,b):
         v1 = pos -a
         v2 = b - a
-        v2 = norm(v2)
+        v2 = self.norm(v2)
         sp = np.dot(v1,v2)
         return (a+v2*sp).astype(int),sp
     
